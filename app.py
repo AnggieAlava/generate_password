99a75abc0c05088f6971fc30b0b936855b16ab0e
Anggie Alava <codewithangels@gmail.com>
2024-08-26 17:12:42 -0500

    Simplificado el test

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ def generate_password(length: int = 16) -> str:
 
 if __name__ == "__main__":
     print(generate_password())
+
+
+# Generación del UUID: uuid.uuid4()  = 4e6f6c75-9fa4-41b9-80d3-8d5e69c73c0b
+
+# Conversión a Bytes: uuid.uuid4().bytes = b'Nolu\x9f\xa4A\xb9\x80\xd3\x8d^i\xc7<\x0b'
+
+# Codificación en Base64 = Tm9sdWn6RBudwNw1Ym0DBw==
